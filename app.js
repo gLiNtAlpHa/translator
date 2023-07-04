a87c623cac9403a354b6b9475b6d5376aec2f1a2
gLiNtAlpHa <sesaalolu@gmail.com>
2023-07-04 03:24:09 +0100

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import r from "./routes/tranRoutes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1/translate", r);
 app.listen(env.port || 5000, () => {
